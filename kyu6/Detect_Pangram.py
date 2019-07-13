245a9c430fbc6e36dc58451abff00b5f77f95d0a
ldunbar3 <ldunbar@mac.com>
2019-07-12 21:37:29 -0400

    Detect Pangram Refactor

--- kyu6/Detect_Pangram.py
+++ kyu6/Detect_Pangram.py
@@ -4,14 +4,10 @@ import string
 
 def is_pangram(s):
     s = s.replace(" ", "").upper()
-    print(s, len(set(s)))
-
     string = ""
     for c in s:
         if c.isalpha():
             string = string + c
-    print(string, "length: ", len(set(string)))
-    print(s)
     return True if len(set((string))) == 26 else False
 
 print(is_pangram("The quick, brown fox jumps over the lazy dog!"))
