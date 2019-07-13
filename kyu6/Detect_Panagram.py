6fda14d1c1aaa8c1ec92adc9e9f1d26a5029ecc5
ldunbar3 <ldunbar@mac.com>
2019-07-12 20:11:03 -0400

    Detect Pangram

--- kyu6/Detect_Panagram.py
+++ kyu6/Detect_Panagram.py
@@ -8,9 +8,8 @@ def is_pangram(s):
     for c in s:
         if c.isalpha():
             string = string + c
-    print(string, " ", len(sorted(set(string))))
+    print(string, "length: ", len(set(string)))
     print(s)
-    return True if len(sorted(set((string)))) == 26 else False
+    return True if len(set((string))) == 26 else False
 
 print(is_pangram("The quick, brown fox jumps over the lazy dog!"))
-print(is_pangram("A gopro"))
