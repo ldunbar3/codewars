be82d16d2b971c6fac46e4e53d372a05f435d227
ldunbar3 <ldunbar@mac.com>
2019-07-13 11:28:47 -0400

    Counting Duplicates

---
+++ kyu6/Counting_Duplicates.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python
+
+def duplicate_count(text):
+    result = []
+    text = text.upper()
+    for i in text:
+        if text.count(i) > 1 and i not in result:
+            result.append(i)
+    return len(result)
+
+print(duplicate_count('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'))
