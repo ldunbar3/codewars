8d85959e36eca3f56c7ae57549f2a0ae8fc11685
ldunbar3 <ldunbar@mac.com>
2019-07-14 10:24:27 -0400

    Sums of Parts refactor

--- kyu6/Sums_Of_Parts.py
+++ kyu6/Sums_Of_Parts.py
@@ -1,9 +1,8 @@
 # https://preview.codewars.com/kata/sums-of-parts/train/python
 
 def parts_sums(ls):
-    result = []
+    result = [sum(ls)]
     total = sum(ls)
-    result.append(total)
     for i in ls:
         total = total - i
         result.append(total)
