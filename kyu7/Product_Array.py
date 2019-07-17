ba5e08426c42b906c661683dee9a83f36e90ea4a
ldunbar3 <ldunbar@mac.com>
2019-07-17 11:25:51 -0400

    cleanup

--- kyu7/Product_Array.py
+++ kyu7/Product_Array.py
@@ -9,10 +9,3 @@ def product_array(numbers):
             p *= i
         product_arr.append(p)
     return product_arr
-
-
-print(product_array([12,20]))#, [20,12])
-print(product_array([12,20]))#, [20,12])
-print(product_array([3,27,4,2]))#, [216,24,162,324])
-print(product_array([13,10,5,2,9]))#, [900,1170,2340,5850,1300])
-print(product_array([16,17,4,3,5,2]))#, [2040,1920,8160,10880,6528,16320])
