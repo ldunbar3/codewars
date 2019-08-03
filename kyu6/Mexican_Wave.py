32206151da565eb1144c360edb1c8c121906b671
ldunbar3 <ldunbar@mac.com>
2019-08-02 22:24:29 -0400

    Mexican wave

--- kyu6/Mexican_Wave.py
+++ kyu6/Mexican_Wave.py
@@ -5,4 +5,8 @@ def wave(str):
     for key, val in enumerate(str):
         if val.isalpha():
             result.append(str[:key] + str[key].upper() + str[key+1:])
+            # result.append(f"{str[:key]}{str[key].upper()}{str[key+1:]}") <- works in my environment, not on codewars
+
     return result
+
+print(wave("This has spaces"))
