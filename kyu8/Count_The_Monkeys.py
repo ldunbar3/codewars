b0bfc9e10123a9e30b9891beff906cbd90a9181f
ldunbar3 <ldunbar@mac.com>
2019-08-05 20:04:49 -0400

    Count the monkeys

---
+++ kyu8/Count_The_Monkeys.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/count-the-monkeys/train/python
+
+'''
+Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+'''
+
+def monkey_count(n):
+    return list(range(1,n+1))
