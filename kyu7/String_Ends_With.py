e64c6948d8e3b4208acd2d33db5b362f7baac175
ldunbar3 <ldunbar@mac.com>
2019-08-05 20:29:00 -0400

    Refactor

--- kyu7/String_Ends_With.py
+++ kyu7/String_Ends_With.py
@@ -1,16 +1,4 @@
 # https://www.codewars.com/kata/string-ends-with/train/python
 
 def solution(string, ending):
-
-    # print(string[-len(ending):])
-    # if string[-len(ending):] == ending:
-    #     return True
-
     return True if string[-len(ending):] == ending else False
-
-
-
-
-
-print(solution('abcde', 'cde'))#, True)
-print(solution('abcde', 'abc'))#, False)
