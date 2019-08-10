aadfebd5d8cab85d834c026df963b4a18bcf6dd7
ldunbar3 <ldunbar@mac.com>
2019-08-09 22:47:06 -0400

    Sum array

---
+++ kyu8/Sum_Array.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/sum-arrays/train/python
+
+'''
+Write a method sum (sum_array in python, sumarray in julia, SumArray in C#) that 
+takes an array of numbers and returns the sum of the numbers. These may be integers 
+or decimals for Ruby and any instance of Num for Haskell. The numbers can also be 
+negative. If the array does not contain any numbers then you should return 0.
+'''
+
+def sum_array(a):
+    return sum(a)
