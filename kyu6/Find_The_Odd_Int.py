cb97ca4c78928b2ea81c969bb2f6637b86f1eb0f
ldunbar3 <ldunbar@mac.com>
2019-08-09 22:35:28 -0400

    Find it

--- kyu6/Find_The_Odd_Int.py
+++ kyu6/Find_The_Odd_Int.py
@@ -1,8 +1,12 @@
 # https://www.codewars.com/kata/find-the-odd-int/train/python
 
+'''
+Given an array, find the int that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+'''
+
 def find_it(seq):
     for i in list(set(seq)):
         if seq.count(i) % 2 != 0:
             return i
-
-print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))#, 5)
