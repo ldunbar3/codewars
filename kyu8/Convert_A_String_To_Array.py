1c3a61d4504d36b772847d0868f3df0f676c56e5
ldunbar3 <ldunbar@mac.com>
2019-08-14 22:37:49 -0400

    Convert string to array

---
+++ kyu8/Convert_A_String_To_Array.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/convert-a-string-to-an-array/train/python
+
+'''
+Write a function to split a string and convert it into an array of words. For example:
+'''
+
+def string_to_array(s):
+    return s.split(" ")
