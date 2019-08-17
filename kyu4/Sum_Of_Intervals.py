39c004eacb60e235066c04e9366fb2d62112ac20
ldunbar3 <ldunbar@mac.com>
2019-08-17 19:44:08 -0400

    Who likes it

--- kyu4/Sum_Of_Intervals.py
+++ kyu4/Sum_Of_Intervals.py
@@ -6,8 +6,3 @@ def sum_of_intervals(intervals):
         for j in range(i[0],i[1]):
             result.append(j)
     return len(set(result))
-
-print(sum_of_intervals([(1, 5)]))#, 4)
-print(sum_of_intervals([(1, 5), (6, 10)]))#, 8)
-print(sum_of_intervals([(1, 5), (1, 5)]))#, 4)
-print(sum_of_intervals([(1, 4), (7, 10), (3, 5)]))#, 7)
--- kyu6/Who_Likes_It.py
+++ kyu6/Who_Likes_It.py
@@ -2,7 +2,6 @@
 
 def likes(names):
     total_likes = len(names)
-
     if total_likes == 0:
         return "no one likes this"
     elif total_likes == 1:
@@ -13,10 +12,3 @@ def likes(names):
         return f"{names[0]}, {names[1]} and {names[2]} like this"
     elif total_likes > 3:
         return  f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
-
-
-print(likes([]))#, 'no one likes this')
-print(likes(['Peter']))#, 'Peter likes this')
-print(likes(['Jacob', 'Alex']))#, 'Jacob and Alex like this')
-print(likes(['Max', 'John', 'Mark']))#, 'Max, John and Mark like this')
-print(likes(['Alex', 'Jacob', 'Mark', 'Max']))#, 'Alex, Jacob and 2 others like this')
