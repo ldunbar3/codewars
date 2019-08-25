1cd57dd0988bb2e036e3f3d766dc2323280c4bc9
ldunbar3 <ldunbar@mac.com>
2019-08-25 10:43:54 -0400

    String Transformer refactor

--- kyu6/String_Transformer.py
+++ kyu6/String_Transformer.py
@@ -1,5 +1,13 @@
 # https://www.codewars.com/kata/string-transformer/train/python
 
+'''
+Given a string, return a new string that has transformed based on the input:
+
+1. Change case of every character, ie. lower case to upper case, upper case to lower case.
+2. Reverse the order of words from the input.
+
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+'''
+
 def string_transformer(s):
-    s = s.split(" ")
-    return ' '.join(reversed(s)).swapcase()
+    return ' '.join(reversed(s.split(" "))).swapcase()
