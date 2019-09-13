6ca4efed1b2b5061f052a9aaf4dae08e908eaf93
ldunbar3 <ldunbar@mac.com>
2019-09-13 11:56:23 +0300

    Consecutive Strings

--- kyu6/Consecutive_Strings.py
+++ kyu6/Consecutive_Strings.py
@@ -4,15 +4,21 @@
 You are given an array strarr of strings and an integer k. Your task is
 to return the first longest string consisting of k consecutive strings taken in the array.
 '''
+
 def longest_consec(strarr, k):
-    to_beat = ""
-    if k >= 0:
-        for i in range(len(strarr)):
-            while i <= len(strarr)-k:
-                current_string = ''.join(strarr[i:i+k])
-                if len(current_string) > len(to_beat):
-                    to_beat = current_string
-                break
-    else:
-        return to_beat
-    return to_beat
+    n = len(strarr)
+    if k <= 0 or k > n or n == 0:
+        return ""
+
+    last_start_idx = n - k
+
+    longest_comp = ""
+    longest_comp_length = 0
+
+    for idx, substring in enumerate(strarr[:last_start_idx + 1]):
+        composite = "".join(strarr[idx:idx + k])
+        composite_length = len(composite)
+        if composite_length > longest_comp_length:
+            longest_comp, longest_comp_length = composite, composite_length
+
+    return longest_comp
