b633f30a967404e9f20daa8c41283a57bfe346b1
ldunbar3 <ldunbar@mac.com>
2019-09-17 18:03:40 -0400

    Spacify

---
+++ kyu7/Spacify.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/spacify/train/python
+
+'''
+Modify the spacify function so that it returns the given string with 
+spaces insertedbetween each character.
+'''
+
+def spacify(string):
+    return ' '.join(list(string))
+
