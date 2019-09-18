92da6cfcaecf52e5072c06a686213bca7dd1890c
ldunbar3 <ldunbar@mac.com>
2019-09-18 09:23:33 -0400

    Alternating Case

---
+++ kyu8/Alternating_Case.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/alternating-case-%3C-equals-%3E-alternating-case/train/python
+
+'''
+Define String.prototype.toAlternatingCase (or a similar function/method such as 
+to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the 
+initial solution for details) such that each lowercase letter becomes uppercase and each uppercase 
+letter becomes lowercase. For example:
+'''
+
+def to_alternating_case(string):
+    return string.swapcase()
