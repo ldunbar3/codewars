ef37654dc2f385742ed36d2fc674bf4feb8ff2c0
ldunbar3 <ldunbar@mac.com>
2019-09-29 10:07:43 -0400

    Select Distinct

---
+++ kyu8/Select_Distinct.sql
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/sql-basics-simple-distinct/train/sql
+
+'''
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+'''
+
+select DISTINCT age from people;
